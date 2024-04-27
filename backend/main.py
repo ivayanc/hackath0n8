16b878b30471f85f2d6422cb5602c8ddb0f8d4b9
ivayanc <ivanyanchenko.odb@gmail.com>
2024-04-27 23:03:24 +0300

    Add cors support

--- backend/main.py
+++ backend/main.py
@@ -21,9 +21,13 @@ from utils.auth import hash_pass, verify_password, create_access_token, create_r
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

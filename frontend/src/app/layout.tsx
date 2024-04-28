0bb742cc63c80230fad75520530ab208bd50a91f
ivayanc <ivanyanchenko.odb@gmail.com>
2024-04-28 05:02:35 +0300

    Add frontend

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+import '../styles/layout/layout.scss';
+import '../styles/demo/Demos.scss';
+
+import { PrimeReactProvider } from 'primereact/api';
+
+import { LayoutProvider } from '@/layout/context/layoutcontext';
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          id="theme-css"
+          href="/themes/lara-light-indigo/theme.css"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <PrimeReactProvider>
+          <LayoutProvider>{children}</LayoutProvider>
+        </PrimeReactProvider>
+      </body>
+    </html>
+  );
+}

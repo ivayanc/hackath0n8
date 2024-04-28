9afe2cd688bd234922f38aba06e31cef76b39c89
ivayanc <ivanyanchenko.odb@gmail.com>
2024-04-28 03:23:58 +0300

    Finish dashboard

--- backend/models/dashboard.py
+++ backend/models/dashboard.py
@@ -8,8 +8,15 @@ class DayStatistic(BaseModel):
     cnt: int
 
 
+class CategoryStatistic(BaseModel):
+    category_name: str
+    procent: int
+
+
 class DashboardDTO(BaseModel):
     new_cnt: int
     in_progress_cnt: int
     completed_cnt: int
+    total_cnt: int
     day_statistic: list[DayStatistic]
+    category_statistic: list[CategoryStatistic]
--- backend/routers/dashboard.py
+++ backend/routers/dashboard.py
@@ -10,7 +10,7 @@ from database.base import Session
 from database.models.request import Request
 from database.models.user import User
 
-from models.dashboard import DashboardDTO, DayStatistic
+from models.dashboard import DashboardDTO, DayStatistic, CategoryStatistic
 
 from utils.database import get_db
 from utils.auth import get_current_user
@@ -32,12 +32,30 @@ async def dashboard_main(user: User = Depends(get_current_user), db: Session = D
             'day': start_date.strftime('%d.%m.%Y'),
             'cnt': len(db.query(Request).filter(sq.func.DATE(Request.created_at) == start_date.date()).all())
         })
+    new_cnt = len(db.query(Request).filter(Request.status == RequestStatus.NEW.value).all())
+    in_progress_cnt = len(db.query(Request).filter(Request.status == RequestStatus.IN_PROGRESS.value,
+                                                 Request.volunteer_id == user.id).all())
+    completed_cnt = len(db.query(Request).filter(Request.status == RequestStatus.COMPLETED.value,
+                                                 Request.volunteer_id == user.id).all())
+    total_cnt = new_cnt + in_progress_cnt + completed_cnt
+
+    category_statistic = {}
+    for request in db.query(Request).all():
+        category_statistic[request.type] = category_statistic.get(request.type, 0) + 1
+    resp = []
+    for category_name in category_statistic:
+        value = category_statistic[category_name]
+        resp.append({
+            'category_name': category_name,
+            'procent': int(value / total_cnt * 100)
+        })
+    resp = sorted(resp, key=lambda x: x['procent'], reverse=True)
 
     return DashboardDTO(
-        new_cnt=len(db.query(Request).filter(Request.status == RequestStatus.NEW.value).all()),
-        in_progress_cnt=len(db.query(Request).filter(Request.status == RequestStatus.IN_PROGRESS.value,
-                                                 Request.volunteer_id == user.id).all()),
-        completed_cnt=len(db.query(Request).filter(Request.status == RequestStatus.COMPLETED.value,
-                                                 Request.volunteer_id == user.id).all()),
-        day_statistic=[DayStatistic(**statistic) for statistic in day_statistic]
+        new_cnt=new_cnt,
+        in_progress_cnt=in_progress_cnt,
+        completed_cnt=completed_cnt,
+        total_cnt=total_cnt,
+        day_statistic=[DayStatistic(**statistic) for statistic in day_statistic],
+        category_statistic=[CategoryStatistic(**statistic) for statistic in resp]
     )
